1a0df18d0b52cf5611db1d5b52c4b826d8991e47
uasr208 <arunsinghutkarsh10@gmail.com>
2026-08-26 18:55:59 +0530

    feat:user cart feature add increase and delete func.. has been added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { fetcher } from "../lib/fetcher";
+import useSWR from "swr";
+import Loader from "./shared/Loader";
+import Error from "./shared/Error";
+import { Button, Card, Tag } from "antd";
+import { priceCalculator } from "../lib/price-calculator";
+import { ShoppingCart } from "lucide-react";
+import { httpRequest } from "../lib/http-request";
+import { toast } from "react-toastify";
+import { useAuth } from "../zustand/useAuth";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { data, error, isLoading } = useSWR("/products", fetcher);
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const addToCart = async (id) => {
+    try {
+      if (!user || user.role !== "user") {
+        navigate("/login");
+        return;
+      }
+      const { data } = await httpRequest.post("/cart", { product: id });
+      toast.success(data.message);
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+  };
+  if (isLoading) return <Loader />;
+
+  if (error) return <Error message={error.message} />;
+
+  return (
+    <div className="w-9/12 mx-auto py-16 grid grid-cols-4 gap-8">
+      {data.map((item, index) => (
+        <Card
+          className="bg-pink-700"
+          key={index}
+          hoverable
+          cover={
+            <img
+              src="/images/product-placeholder.jpg"
+              className="h-60 object-cover"
+            />
+          }
+        >
+          <Card.Meta
+            title={item.title}
+            description={
+              <div className="flex items-center gap-2">
+                <label className="font-medium text-gray-600">
+                  ₹{priceCalculator(item.price, item.discount)}
+                </label>
+                <del className="text-rose-500">
+                  ₹{item.price.toLocaleString()}
+                </del>
+                <label>({item.discount}% Discount)</label>
+              </div>
+            }
+          />
+          <Tag className="!mt-3">Men's Clothing</Tag>
+          <div className="mt-4 space-x-3 flex flex-col">
+            <Button
+              onClick={() => addToCart(item._id)}
+              size="large"
+              type="primary"
+              icon={<ShoppingCart />}
+            >
+              Add to cart
+            </Button>
+          </div>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
--- src/components/user/UserCarts.jsx
+++ src/components/user/UserCarts.jsx
@@ -1,7 +1,118 @@
+import { Button, Card, Popconfirm } from "antd";
+import { Minus, Plus, ShoppingCart, Trash2 } from "lucide-react";
 import React from "react";
+import useSWR, { mutate } from "swr";
+import { fetcher } from "../../lib/fetcher";
+import Loader from "../shared/Loader";
+import { priceCalculator } from "../../lib/price-calculator";
+import ButtonGroup from "antd/es/button/ButtonGroup";
+import { httpRequest } from "../../lib/http-request";
+import { toast } from "react-toastify";
 
 const UserCarts = () => {
-  return <div>UserCarts</div>;
+  const { data, error, isLoading } = useSWR("/cart", fetcher);
+
+  const increaseDecreaseCart = async (id, qnt) => {
+    try {
+      await httpRequest.put(`/cart/${id}`, { qnt: qnt });
+      mutate("/cart");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  const deleteCart = async (id) => {
+    try {
+      await httpRequest.delete(`/cart/${id}`);
+      mutate("/cart");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+  if (isLoading) return <Loader />;
+
+  if (error) return <Error message={error.message} />;
+  return (
+    <div>
+      <Card className="shadow-lg ">
+        <Card.Meta
+          title={
+            <div className="flex items-center gap-2 ">
+              <ShoppingCart />
+              <h1>Shopping Carts</h1>
+            </div>
+          }
+        />{" "}
+        <div className="mt-8">
+          {data.map((item, index) => (
+            <Card hoverable key={index}>
+              <div className="flex items-start justify-between">
+                <div className="flex items-center gap-4">
+                  <img
+                    src={item.image || "/images/product-placeholder.jpg"}
+                    className="w-34 rounded-lg"
+                  />
+                  <h1 className="capitalize font-semibold text-lg">
+                    {item.product.title}
+                  </h1>
+                  <p className="text-gray-500">
+                    {item.product.description.slice(0, 100)}...
+                  </p>
+
+                  <div className="flex items-center gap-2 mt-2">
+                    <label className="font-medium text-gray-600">
+                      ₹
+                      {priceCalculator(
+                        item.product.price,
+                        item.product.discount,
+                      )}
+                    </label>
+                    <del className="text-rose-500">
+                      ₹{item.product.price.toLocaleString()}
+                    </del>
+                    <label>({item.product.discount}% Discount)</label>
+                  </div>
+                  <Popconfirm
+                    title="do you want to delete this cart?"
+                    onConfirm={() => deleteCart(item._id)}
+                  >
+                    <Button
+                      className="mt-3"
+                      icon={<Trash2 className="w-4 h-4 " />}
+                      danger
+                      type="primary"
+                    >
+                      Delete
+                    </Button>
+                  </Popconfirm>
+                </div>
+
+                <div className="flex items-start">
+                  <Card title="Quantity">
+                    <ButtonGroup>
+                      <Button
+                        icon={<Minus className="w-4 h-4" />}
+                        onClick={() =>
+                          increaseDecreaseCart(item._id, item.qnt - 1)
+                        }
+                      />
+                      <Button>{item.qnt}</Button>
+                      <Button
+                        icon={<Plus className="w-4 h-4" />}
+                        onClick={() =>
+                          increaseDecreaseCart(item._id, item.qnt + 1)
+                        }
+                      />
+                    </ButtonGroup>
+                  </Card>
+                </div>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default UserCarts;
